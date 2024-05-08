642d7996ad17829adaea39f05ffeceed86e4a9d1
Neotin695 <Mehani695@gmail.com>
2024-05-08 16:08:53 -0700

    fix last 2 column removed from data

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import pandas as pd
 
 from sklearn.preprocessing import LabelEncoder
+from model import encoder
 
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
@@ -24,19 +25,19 @@ def process_array():
     features = np.array([array_data])
     predicted_labels = model.predict(features)
 
-    data = pd.read_csv("medical_data.csv").iloc[:, :-2]
-    features_name = list(data['spicitalist'])
-    features_name.append('spicitalist')
-    class_names = data.columns[1:]
-    data_reshaped = data.drop(['spicitalist'], axis=1)
-    data_reshaped.loc[len(data_reshaped.index)] = class_names
-
-    data_reshaped = pd.DataFrame(data_reshaped.values.T, columns=features_name)
-    # تحديد المتغيرات المستقلة (الأعراض) والمستهدفة (التخصص الطبي)
-    X = data_reshaped.drop(['spicitalist'], axis=1)
-    encoder = LabelEncoder()
-    y = encoder.fit_transform(data_reshaped['spicitalist'])
-    # يمكنك طباعة التصنيفات المتوقعة للتحقق منها
+    # data = pd.read_csv("medical_data.csv").iloc[:, :-2]
+    # features_name = list(data['spicitalist'])
+    # features_name.append('spicitalist')
+    # class_names = data.columns[1:]
+    # data_reshaped = data.drop(['spicitalist'], axis=1)
+    # data_reshaped.loc[len(data_reshaped.index)] = class_names
+    #
+    # data_reshaped = pd.DataFrame(data_reshaped.values.T, columns=features_name)
+    # # تحديد المتغيرات المستقلة (الأعراض) والمستهدفة (التخصص الطبي)
+    # X = data_reshaped.drop(['spicitalist'], axis=1)
+    # encoder = LabelEncoder()
+    # y = encoder.fit_transform(data_reshaped['spicitalist'])
+    # # يمكنك طباعة التصنيفات المتوقعة للتحقق منها
     print("Predicted Labels:", predicted_labels)
     # encoder = LabelEncoder()
     predicted_labels_names = encoder.inverse_transform(predicted_labels)
